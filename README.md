759b0652ca2de39971c2db7bcfc33917abd8b3ef
trystan2k <trystan2k@gmail.com>
2020-03-08 11:21:07 +0100

    feat(*): add npm plugin files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# zsh-npm-plugin
-A ZSH plugin to set custom NPM aliases. Is based (forked) from Oh-My-ZSH one.
+# ZSH NPM Plugin
+
+[![v1.0.0](https://img.shields.io/badge/version-1.1.0-brightgreen.svg)](https://github.com/trystan2k/zsh-npm-plugin/tree/v1.0.0)
+
+A zsh plugin that add NPM custom alias
+
+This is completely based on [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/npm) NPM plugin.
+
+## Installation
+
+### Using [zinit](https://github.com/zdharma/zinit)
+
+Add `zinit light trystan2k/zsh-npm-plugin` into `.zshrc`
+
+### Using [zpm](https://github.com/zpm-zsh/zpm)
+
+Add `zpm load trystan2k/zsh-npm-plugin` into `.zshrc`
+
+### Using [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh)
+
+Execute `git clone https://github.com/trystan2k/zsh-npm-plugin ~/.oh-my-zsh/custom/plugins/zsh-npm-plugin`. Add `zsh-npm-plugin` into plugins array in `.zshrc`
+
+### Using [antigen](https://github.com/zsh-users/antigen)
+
+Add `antigen bundle trystan2k/zsh-npm-plugin` into `.zshrc`
+
+### Using [zgen](https://github.com/tarjoilija/zgen)
+
+Add `zgen load trystan2k/zsh-npm-plugin` into `.zshrc`
---
+++ npm.plugin.sh
@@ -0,0 +1,65 @@
+# Install dependencies globally
+alias npmg="npm i -g "
+
+# Install package / dependencies
+alias npmi="npm i"
+
+# npm package names are lowercase
+# Thus, we've used camelCase for the following aliases:
+
+# Install and save to dependencies in your package.json
+# npms is used by https://www.npmjs.com/package/npms
+alias npmS="npm i -S "
+
+# Install and save to dev-dependencies in your package.json
+# npmd is used by https://github.com/dominictarr/npmd
+alias npmD="npm i -D "
+
+# Execute command from node_modules folder based on current directory
+# i.e npmE gulp
+alias npmE='PATH="$(npm bin)":"$PATH"'
+
+# Check which npm modules are outdated
+alias npmO="npm outdated"
+
+# Check package versions
+alias npmV="npm -v"
+
+# List packages
+alias npmL="npm list"
+
+# List top-level installed packages
+alias npmL0="npm ls --depth=0"
+
+# List packages globally
+alias npmlsg="npm list -g"
+
+# List top-level installed packages globally
+alias npmlsg0="npm list -g --depth=0"
+
+# Run npm start
+alias npmst="npm start"
+
+# Run npm test
+alias npmt="npm test"
+
+# Run npm scripts
+alias npmR="npm run"
+
+# Run npm publish 
+alias npmP="npm publish"
+
+# Run npm init
+alias npmNew="npm init"
+
+# Clear node_modules in current directory
+# ! for prevent accidental run
+alias npmclr!="rm -rf ./node_modules/"
+
+# Reinstall package
+# ! for prevent accidental run
+alias nre!="npmclr! && npmi"
+
+# Search in the npm database
+alias npmF="npm search"
+
